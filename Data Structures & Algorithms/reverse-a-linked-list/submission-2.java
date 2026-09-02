6ca8c8a6a5261519e2fbd3f238a07cedeb0191a5
Shivam Kumar <144939839+Sh1vamBhaskar@users.noreply.github.com>
2026-09-02 16:04:19 +0530

    Add: reverse-a-linked-list - submission-2

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-2.java
@@ -0,0 +1,21 @@
+
+//Itterative approch to solve below question
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head ;
+
+        while(curr != null) {
+            ListNode frwd = curr.next;
+            curr.next = prev;
+
+            prev = curr;
+            curr = frwd;
+
+        }
+
+        return prev;
+        
+    }
+}
